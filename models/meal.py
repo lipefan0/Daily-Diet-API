2d727b77cf97c21ae25ddf11189084c501074107
Felipe Oliveira <lipefan0@gmail.com>
2024-09-05 14:24:32 -0300

    Updating class Meal

--- models/meal.py
+++ models/meal.py
@@ -4,7 +4,7 @@ class Meal(db.Model):
     # id (int), id_user (int),food (str), description (str), date (date), is_it_a_diet (bool)
     id = db.Column(db.Integer, primary_key=True)
     id_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    food = db.Column(db.String(80), unique=True, nullable=False)
+    food = db.Column(db.String(80), unique=False, nullable=False)
     description = db.Column(db.String(80), nullable=False)
     date = db.Column(db.Date(), nullable=False)
     is_it_a_diet = db.Column(db.Boolean(), nullable=False, default=False)
